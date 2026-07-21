b1ac6510edfe951f028bf15bb3d601e917e3dc01
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-21 10:54:48 +0530

    Upper case and lower Case string

--- UpperCase&lowerCase.py
+++ UpperCase&lowerCase.py
@@ -1,3 +1,4 @@
+# Write a Python  script that takes input from the user and displays that input back in upper and lower cases
 st = input("Enter your string: ")
 print(st.upper())
 print(st.lower())
