773b9f2210221f16082c092c989385036a332ab5
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-22 16:53:10 +0530

    revised input function in python

---
+++ PYTHONBasicpractices/Input.py
@@ -0,0 +1,12 @@
+#python input function
+
+#string input
+name = input("Enter your name: ")
+
+#integer input
+age = int(input("Enter your age: "))
+
+#float  input
+salary = float(input("Enter your salary: "))
+
+print(f'{name}\n{age}\n{salary}')
