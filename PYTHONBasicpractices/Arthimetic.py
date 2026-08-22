51e0bac8a940199fcb203aec59b246cec9f03b08
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-22 16:09:55 +0530

    practice Arthimetic operator in python

---
+++ PYTHONBasicpractices/Arthimetic.py
@@ -0,0 +1,17 @@
+num1 = 54
+num2 = 66
+
+#addition
+print(num1 + num2)
+
+#subtract
+print(num1 - num2)
+
+#multiplication
+print(num1 * num2)
+
+#division 
+print(num1/num2)
+
+#modula
+print(num1%num2)
