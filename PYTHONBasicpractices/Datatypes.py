ffcea1a89cc7715ccb0ca0cad8109b52ef03c629
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-22 11:24:55 +0530

    practice python datatype and type function

---
+++ PYTHONBasicpractices/Datatypes.py
@@ -0,0 +1,32 @@
+# integer
+number = 5 
+
+#string
+name = 'siddharth saxena'
+
+#float
+decimal_value = 4.56
+
+#complex number
+c = 2+4j
+
+print(f'{number}\n{name}\n{decimal_value}\n{c}')
+
+# list 
+list = ["I","am","siddharth"]
+
+#tuple
+tuple = ('sid','saxena')
+
+print(f'{list}\n{tuple}')
+
+#boolean
+bool = True
+
+#set 
+set1 = set('I am set')
+
+#dictionary
+Dict = {1:'python',2:'java'}
+
+print(f'{bool}\n{set1}\n{Dict}')
