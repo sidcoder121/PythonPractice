8989ca4c2f8bbb5d22de1e64aaf836001f74361a
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-22 16:32:50 +0530

    revised membership operator

---
+++ PYTHONBasicpractices/membership.py
@@ -0,0 +1,15 @@
+# not in 
+# in
+x = 24
+y = 20
+list = [10,20,30,40,50,60]
+
+if(x not in list):
+    print("X is not present")
+else:
+    print("X is present in given list")
+
+if(y in list):
+    print("Y is present in given list")
+else:
+    print("Y is not present in given list")
