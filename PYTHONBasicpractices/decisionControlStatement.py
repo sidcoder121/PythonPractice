71e309d0c6dc8a0e0c674c8a76b57e5b1290a28a
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-22 17:12:37 +0530

    revised decision control statement in pyhon

---
+++ PYTHONBasicpractices/decisionControlStatement.py
@@ -0,0 +1,22 @@
+# if
+# if-else
+# nested if
+# if-elif-else
+
+n = 20
+if (n%2 == 0):
+    print('n is even number')
+    
+num = 22
+if (num%2 == 0):
+    print("num is even")
+else:
+    print('num is odd')
+    
+age = 22
+if (age >= 18):
+    if (age>18):
+        print('age is greater than 18')
+    else:
+        print('age is equal to 18')
+        
