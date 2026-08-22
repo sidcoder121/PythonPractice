b170aef1d4f4662ca874193ca7deafbd9e25ca00
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-22 17:29:29 +0530

    start practicing list in python

---
+++ PYTHONBasicpractices/list.py
@@ -0,0 +1,25 @@
+# create a list 
+data = ["Apple","Orange","Grapes"]
+print(data)
+
+# create a list with distinct(duplicate elements)
+list1 = [1,2,'sid']
+print(list1)
+
+#accessing elements from list
+print(data[0])
+print(data[1])
+
+#Take input of python list
+string = input('Enter your list: ')
+list = string.split()
+print("The list is :",list)
+
+
+#adding element in a list
+list2 = [1,2,3]
+print(list2)
+
+list2.append(4)
+list2.append(5)
+print('list after append: ', list2)
