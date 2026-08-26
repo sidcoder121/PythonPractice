69c277017c5d88648569207f6c2c4c4bf8e7667f
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-26 15:46:40 +0530

    Accessing the string element

---
+++ PYTHONBasicpractices/Strings.py
@@ -0,0 +1,14 @@
+#Create a String
+String = "I am Siddharth saxena"
+print(String)
+
+#Multiline String
+String1 = '''Twinkle Twinkle Liitle Star,
+How are wonder who you are'''
+print(String1)
+
+#Accessing character in String 
+String2 = "Good things take time"
+print("length: ",len(String))
+print(String2[0])
+print(String2[3])
