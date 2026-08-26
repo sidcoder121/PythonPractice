3feb743076a1b32109ae8838a2f0898c36d6feb4
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-26 14:31:44 +0530

    Practice Tuple in python

---
+++ PYTHONBasicpractices/tuple.py
@@ -0,0 +1,17 @@
+Tuple1 = tuple('Sid')
+print(Tuple1)
+
+#Accesing tuple with indexing
+print(Tuple1[0])
+
+#Tuple unpacking
+Tuple2 = ("I","am","Ironman")
+a,b,c = Tuple2
+print("value after unpacking \n")
+print(a)
+print(b)
+print(c)
+
+#concatenation of tuple
+Tuple3 = Tuple1 + Tuple2
+print(Tuple3)
