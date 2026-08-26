678dd76a80590f9f5b53ad4d2710fa20fdc9016a
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-26 14:54:38 +0530

    Practice reversed tuple and slicing tuple

--- PYTHONBasicpractices/tuple.py
+++ PYTHONBasicpractices/tuple.py
@@ -15,3 +15,10 @@ print(c)
 #concatenation of tuple
 Tuple3 = Tuple1 + Tuple2
 print(Tuple3)
+
+#Slicing of tuple
+Tuple4 = tuple('Siddhrth')
+print(Tuple4[1:])
+
+#Reversing the list
+print("Reversed Tuple \n", Tuple1[: :-1])
