bc8daf0b90c6477a6a6ccc7f50d1ebb95660ad61
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-26 14:59:18 +0530

    revised range in tuple

--- PYTHONBasicpractices/tuple.py
+++ PYTHONBasicpractices/tuple.py
@@ -17,8 +17,12 @@ Tuple3 = Tuple1 + Tuple2
 print(Tuple3)
 
 #Slicing of tuple
-Tuple4 = tuple('Siddhrth')
+Tuple4 = tuple('Siddharth')
 print(Tuple4[1:])
 
 #Reversing the list
 print("Reversed Tuple \n", Tuple1[: :-1])
+
+#printing elements in range
+print("Elements in range\n")
+print(Tuple4[4:9])
