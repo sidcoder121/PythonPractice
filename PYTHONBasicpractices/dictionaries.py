bebc8ad1e4d0353b13912cd7c2a644de10a0df65
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-28 13:28:28 +0530

    complete dicionary practices

--- PYTHONBasicpractices/dictionaries.py
+++ PYTHONBasicpractices/dictionaries.py
@@ -12,3 +12,23 @@ Dict[2] = 'great'
 
 print("dictionary after adding elements")
 print(Dict)
+
+#Accessing element in a dictionary
+print(Dict[1])
+
+#get() method - it is used to access dictionary safely
+print(Dict.get(2))
+
+#Accessing element of a nested Dictionary
+Dictionary = {'Dict1':{1:'Vartika'},
+        'Dict2':{2:'Siddharth'}}
+
+print(Dictionary['Dict1'])
+print(Dictionary['Dict2'][2])
+
+#Deleting element using del keyword
+Dic = {'name':'Siddharth',1:'Saxena',3:'Guna'}
+print(Dic)
+
+del(Dic[1]) #del keyword
+print(Dic)
