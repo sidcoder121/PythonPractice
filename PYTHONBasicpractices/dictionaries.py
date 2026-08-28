045de7cf99d745faba5b27f7297030bfb6eec4bb
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-28 13:07:52 +0530

    Start practicibg dictionary

---
+++ PYTHONBasicpractices/dictionaries.py
@@ -0,0 +1,14 @@
+dict = {1:'I',2:'am',3:'sid'}
+print(dict)
+
+#Adding elements in dictionary
+Dict = {}
+print("Empty dictionary")
+print(Dict)
+
+Dict[0] = 'Sid'
+Dict[1] = 'is'
+Dict[2] = 'great'
+
+print("dictionary after adding elements")
+print(Dict)
