6736e5b79726864369fc3399bde64993507484cf
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-28 13:37:56 +0530

    practice functions in python

---
+++ PYTHONBasicpractices/functions.py
@@ -0,0 +1,23 @@
+'''def function_name(parameters):
+    #satement
+    return expression'''
+    
+#creating a function
+def func():
+    print('Welcome in function')
+
+func() #call a function
+
+#create a function to add numbers
+def add(num1:int,num2:int):
+    num3 = num1 + num2
+    return num3
+
+print(add(6,4))
+
+#default arguments
+def myfunc(x,y=10):
+    print("x : ",x)
+    print("y : ",y)
+
+myfunc(20)
