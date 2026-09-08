6d1b8eca1b64729addfce144d597202989e5f700
Siddharth Saxena <sidcoder73@gmail.com>
2026-09-08 13:14:51 +0530

    create  a list program using lambda

--- PYTHONBasicpractices/list.py
+++ PYTHONBasicpractices/list.py
@@ -44,3 +44,8 @@ print(list2)
 list2.pop()
 print(list2)
 
+ #Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
+
+lis = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+lis.sort(key=lambda x:x[-1])
+print(lis)
